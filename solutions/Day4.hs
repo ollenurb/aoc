0bd0c40994ddc2f93a0b19d38aebea9020f83821
Ollenurb <brunello.mtt@gmail.com>
2021-12-08 11:03:42 +0100

    Finished Day4

--- solutions/Day4.hs
+++ solutions/Day4.hs
@@ -3,13 +3,14 @@ module Day4 where
 -- Needed imports
 import Data.List
 import Data.Maybe
+import Data.List.Split (splitOn, splitWhen)
 
 -- Do not change. The main program will access the solutions from this function
 solve :: String -> String
-solve fcontent = "Solution 1:\t" ++ sol1 ++ "\nSolution2:\t" ++ sol2 ++ "\n"
+solve fcontent = "Solution 1:\t" ++ sol1 ++ "\nSolution 2:\t" ++ sol2 ++ "\n"
     where problemInput = parseFileContent fcontent
           sol1 = (show . s1) problemInput
-          sol2 = (show . s1) problemInput
+          sol2 = (show . s2) problemInput
 
 ------------------------------------------------------------------------------
 -- Change according to the problem
@@ -17,65 +18,14 @@ type Board a = [[Maybe a]]
 
 type ProblemInput = ([Int], [Board Int])
 
--- Testing Stuff - TODO: Remove
-inSeq :: [Int]
-inSeq = [7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1]
-
-inBoards :: [Board Int]
-inBoards = (map . map . map) Just [
-               [
-                    [22,13,17,11, 0],
-                    [8 ,2 ,23, 4,24],
-                    [21, 9,14,16, 7],
-                    [6 ,10, 3,18, 5],
-                    [ 1,12,20,15,19]
-               ],
-               [
-                    [3,15, 0, 2, 22],
-                    [9,18,13,17, 5 ],
-                    [19, 8, 7,25,23],
-                    [20,11,10,24, 4],
-                    [14,21,16,12, 6]
-               ],
-               [
-                    [14,21,17,24, 4],
-                    [10,16,15, 9,19],
-                    [18, 8,23,26,20],
-                    [22,11,13, 6, 5],
-                    [2 ,0,12, 3, 7]
-               ]
-           ]
-
-
-sampleFileInput :: String
-sampleFileInput = "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1\n\
-\\n\
-\22 13 17 11  0\n\
-\ 8  2 23  4 24\n\
-\21  9 14 16  7\n\
-\ 6 10  3 18  5\n\
-\ 1 12 20 15 19\n\
-\\n\
-\ 3 15  0  2 22\n\
-\ 9 18 13 17  5\n\
-\19  8  7 25 23\n\
-\20 11 10 24  4\n\
-\14 21 16 12  6\n\
-\\n\
-\14 21 17 24  4\n\
-\10 16 15  9 19\n\
-\18  8 23 26 20\n\
-\22 11 13  6  5\n\
-\ 2  0 12  3  7\n"
-
 -- Parse the input file
 parseFileContent :: String -> ProblemInput
-parseFileContent = undefined
-
--- Solve the first part
-s1 :: ProblemInput -> Int
-s1 (rndseq, boards) = (\(x, b)->x * sumUnmarked b) $ findWinner rndseq boards
-    where sumUnmarked = sum . map (sum . catMaybes)
+parseFileContent inputFile = (inSeq, boards)
+    where inputLines = lines inputFile
+          inSeq = map strtoi . splitOn "," . head $ inputLines
+          boards = (map . map) (map (Just . strtoi)  . words) strBoards
+          strBoards = splitWhen (=="") . tail $ inputLines
+          strtoi x = read x :: Int
 
 -- Determines if a board is a win board or not
 isWin :: Board Int -> Bool
@@ -90,15 +40,30 @@ signBoard x = (map . map) (sign (Just x))
             | v == v'   = Nothing
             | otherwise = v'
 
--- Find the first winning board in a board list given a sequence of number
-findWinner :: [Int] -> [Board Int] -> (Int, Board Int)
-findWinner [] _ = undefined
-findWinner (x:xs) boards
-    | isJust winningBoard = (x, fromJust winningBoard)
-    | otherwise = findWinner xs signedBoards
+-- Find the every winning board in a board list given a sequence of number
+findWinners :: [Int] -> [Board Int] -> [(Int, Board Int)]
+findWinners _ [] = []
+findWinners [] _ = []
+findWinners (x:xs) boards = zip (repeat x) winningBoards ++ findWinners xs losingBoards
     where signedBoards = map (signBoard x) boards
-          winningBoard = find isWin signedBoards
+          losingBoards = filter (not . isWin) signedBoards
+          winningBoards = filter isWin signedBoards
+
+boardScore :: Int -> Board Int -> Int
+boardScore x board = x * sumUnmarked board
+    where sumUnmarked = sum . map (sum . catMaybes)
+
+--- Solve the first part
+s1 :: ProblemInput -> Int
+s1 (xs, boards) = boardScore n b
+    where (n, b) = head $ findWinners xs boards
 
 -- Solve the second part
 s2 :: ProblemInput -> Int
-s2 problemInput = -1
+s2 (xs, boards) = boardScore n b
+    where (n, b) = last $ findWinners xs boards
+
+-- TODO: Here are some improvements that can be done
+--    * findWinners can actually return a list of scores by computing scores
+--      beforehand
+--    * Input parsing can be done with a Parser combinator library
