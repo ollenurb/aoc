5005c0f6e51f634abeb8d1c882588784b6550be1
Ollenurb <brunello.mtt@gmail.com>
2021-12-09 01:32:18 +0100

    Day 5 - S1

--- solutions/Day5.hs
+++ solutions/Day5.hs
@@ -2,6 +2,8 @@ module Day5 where
 
 -- Needed imports
 import Data.Tuple (swap)
+import Data.List (group)
+import qualified Data.HashMap as HM
 
 -- Do not change. The main program will access the solutions from this function
 solve :: String -> String
@@ -14,25 +16,53 @@ solve fcontent = "Solution 1:\t" ++ sol1 ++ "\nSolution 2:\t" ++ sol2 ++ "\n"
 data Line = Line { getStart :: (Int, Int), getEnd :: (Int, Int) }
     deriving (Show, Eq)
 
+sampleLines :: [Line]
+sampleLines = [
+    Line (0,9) (5,9),
+    Line (8,0) (0,8),
+    Line (9,4) (3,4),
+    Line (2,2) (2,1),
+    Line (7,0) (7,4),
+    Line (6,4) (2,0),
+    Line (0,9) (2,9),
+    Line (3,4) (1,4),
+    Line (0,0) (8,8),
+    Line (5,5) (8,2)]
+
 -- Change according to the problem
 type ProblemInput = [Line]
 
 -- Parse the input file
 parseFileContent :: String -> ProblemInput
-parseFileContent = undefined
+parseFileContent = map (parseLine . words) . lines
+    where parseLine [s, "->", e] = Line (read $ parenthesize s) (read $ parenthesize e)
+          parseLine _            = error "Malformed line found"
+          parenthesize x = "(" ++ x ++ ")"
 
--- TODO: Can be refactorized
-genVHPoints :: Line -> [(Int, Int)]
-genVHPoints (Line (x, y) (x', y'))
+-- TODO: To remove
+testSolve :: IO Int
+testSolve = do
+    fileContent <- readFile "inputs/Day5.txt"
+    (return . s1 . parseFileContent) fileContent
+
+genPoints :: Line -> [(Int, Int)]
+genPoints (Line (x, y) (x', y'))
   | x == x' && y > y' = zip (repeat x) [y'..y]
   | x == x' && y < y' = zip (repeat x) [y..y']
   | y == y' && x > x' = zip [x'..x] (repeat y)
   | y == y' && x < x' = zip [x..x'] (repeat y)
   | otherwise = [(x, y), (x', y')]
 
+isDiagonal :: Line -> Bool
+isDiagonal (Line (x, y) (x', y')) = x /= x' && y /= y'
+
 -- Solve the first part
 s1 :: ProblemInput -> Int
-s1 problemInput = -1
+s1 problemInput = length $ filter ((>= 2) . snd) nOverlaps
+    where hvLines = filter (not . isDiagonal) problemInput
+          linesPoints = hvLines >>= genPoints
+          nOverlaps = (HM.toList . groupAll) linesPoints
+          groupAll = foldr (\x->HM.insertWith (+) x 1) HM.empty
 
 -- Solve the second part
 s2 :: ProblemInput -> Int
