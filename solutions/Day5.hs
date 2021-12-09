bf6426c348d9d89f61fddb2e9f7fb4c09390528e
Ollenurb <brunello.mtt@gmail.com>
2021-12-09 23:00:50 +0100

    Gigachad @giuseppe998e

--- solutions/Day5.hs
+++ solutions/Day5.hs
@@ -10,7 +10,7 @@ solve :: String -> String
 solve fcontent = "Solution 1:\t" ++ sol1 ++ "\nSolution 2:\t" ++ sol2 ++ "\n"
     where problemInput = parseFileContent fcontent
           sol1 = (show . s1) problemInput
-          sol2 = (show . s1) problemInput
+          sol2 = (show . s2) problemInput
 
 ------------------------------------------------------------------------------
 data Line = Line { getStart :: (Int, Int), getEnd :: (Int, Int) }
@@ -45,16 +45,27 @@ testSolve = do
     fileContent <- readFile "inputs/Day5.txt"
     (return . s1 . parseFileContent) fileContent
 
+(...) :: (Ord a, Enum a) => a ->  a -> [a]
+a ... b
+  | a < b   = [a..b]
+  | otherwise = reverse [b..a]
+
 genPoints :: Line -> [(Int, Int)]
 genPoints (Line (x, y) (x', y'))
-  | x == x' && y > y' = zip (repeat x) [y'..y]
-  | x == x' && y < y' = zip (repeat x) [y..y']
-  | y == y' && x > x' = zip [x'..x] (repeat y)
-  | y == y' && x < x' = zip [x..x'] (repeat y)
-  | otherwise = [(x, y), (x', y')]
+  | y == y'            = zip (x...x') (repeat y)
+  | x == x'            = zip (repeat x) (y...y')
+  | otherwise          = zip (x...x') (y...y')
 
 isDiagonal :: Line -> Bool
 isDiagonal (Line (x, y) (x', y')) = x /= x' && y /= y'
+
+is45Deg :: Line -> Bool
+is45Deg (Line (x, y) (x', y')) = abs (x - x') == abs (y - y')
+
+-- Given a list of points, returns an hashmap that indicates how many times
+-- a point occur on the original list
+groupAll :: [(Int, Int)] -> HM.Map (Int, Int) Int
+groupAll = foldr (\x->HM.insertWith (+) x 1) HM.empty
 
 -- Solve the first part
 s1 :: ProblemInput -> Int
@@ -62,8 +73,12 @@ s1 problemInput = length $ filter ((>= 2) . snd) nOverlaps
     where hvLines = filter (not . isDiagonal) problemInput
           linesPoints = hvLines >>= genPoints
           nOverlaps = (HM.toList . groupAll) linesPoints
-          groupAll = foldr (\x->HM.insertWith (+) x 1) HM.empty
 
 -- Solve the second part
 s2 :: ProblemInput -> Int
-s2 problemInput = -1
+s2 problemInput = length $ filter ((>= 2) . snd) nOverlaps
+    where hvLines = filter (not . isDiagonal) problemInput
+          lines45 = filter is45Deg problemInput
+          linesPoints = (hvLines ++ lines45) >>= genPoints
+          nOverlaps = (HM.toList . groupAll) linesPoints
+
