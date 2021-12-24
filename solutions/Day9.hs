441d2faa2278ae1f27b1309b70f80c498e19f748
Ollenurb <brunello.mtt@gmail.com>
2021-12-24 15:22:27 +0100

    Day9 - Part 2

--- solutions/Day9.hs
+++ solutions/Day9.hs
@@ -3,6 +3,9 @@ module Day9 where
 -- Needed imports
 import Data.Char
 import Data.Maybe
+import Data.List (sortBy)
+import Data.Ord
+import qualified Data.Set as S
 import qualified Data.Matrix as M
 
 -- Do not change. The main program will access the solutions from this function
@@ -15,6 +18,9 @@ solve fcontent = "Solution 1:\t" ++ sol1 ++ "\nSolution 2:\t" ++ sol2 ++ "\n"
 ------------------------------------------------------------------------------
 -- Change according to the problem
 type ProblemInput = M.Matrix Int
+type HeightMap = M.Matrix Int
+type Coord = (Int, Int)
+
 -- Parse the input file
 parseFileContent :: String -> ProblemInput
 parseFileContent = M.fromLists . map (map digitToInt) . lines
@@ -26,22 +32,36 @@ sampleInput = M.fromLists [[2,1,9,9,9,4,3,2,1,0],
                            [8,7,6,7,8,9,6,7,8,9],
                            [9,8,9,9,9,6,5,6,7,8]]
 
--- cmpList :: M.Matrix Int -> (Int, Int) -> Int -> Bool
--- cmpList m (i,j) e = all (< e) adjacent
---     where stencil = [(0, 1), (0, -1), (1, 0), (-1, 0)]
---           adjacent = catMaybes [M.safeGet (i+i') (j+j') m | (i', j') <- stencil]
+allowedDirs :: [Coord]
+allowedDirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
-riskLevel :: M.Matrix Int -> (Int, Int) -> Int -> Int
-riskLevel m (i,j) e
-    | all (> e) adjacent = e+1
-    | otherwise          = 0
-    where stencil = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-          adjacent = catMaybes [M.safeGet (i+i') (j+j') m | (i', j') <- stencil]
+-- Given an element and its position, returns if it is a low point or not
+isLowPoint :: HeightMap -> Coord -> Int -> Bool
+isLowPoint m (i, j) e = all (> e) adjacent
+    where adjacent = catMaybes [M.safeGet (i+i') (j+j') m | (i', j') <- allowedDirs]
+
+-- Find low points
+findLowPoints :: HeightMap -> [Coord]
+findLowPoints m = (catMaybes  . M.toList . M.mapPos (collect m)) m
+    where collect m p e = if isLowPoint m p e then Just p else Nothing
 
 -- Solve the first part
 s1 :: ProblemInput -> Int
-s1 pi = (sum . M.toList) $ M.mapPos (riskLevel pi) pi
+s1 pi = foldr f 0 $ findLowPoints pi
+    where f (i, j) a = M.getElem i j pi + 1 + a
 
--- Solve the second part
+basin :: HeightMap -> S.Set Coord -> Coord -> S.Set Coord
+basin m s (i, j)
+  | e == 9    = s
+  | otherwise = (i, j) `S.insert` curBasin
+  where adjacentPos = (S.fromList [(i+i', j+j') | (i', j') <- allowedDirs]) S.\\ s
+        e = fromMaybe 9 $ M.safeGet i j m
+        m' = M.setElem 9 (i, j) m
+        curBasin = foldl evalNeighbs S.empty adjacentPos
+        evalNeighbs a p = a `S.union` basin m' (a `S.union` s) p
+
 s2 :: ProblemInput -> Int
-s2 problemInput = -1
+s2 pi = product $ take 3 (basinsSort lps)
+    where lps = findLowPoints pi
+          basinsSort = reverseSort . map (S.size . basin pi S.empty)
+          reverseSort = sortBy (\x y -> Down x `compare` Down y)
