32560595c40c59ea3c00bb4486ad0ebd29548f80
Ollenurb <brunello.mtt@gmail.com>
2021-12-24 15:23:15 +0100

    Cleanup Day 9

--- solutions/Day9.hs
+++ solutions/Day9.hs
@@ -25,13 +25,6 @@ type Coord = (Int, Int)
 parseFileContent :: String -> ProblemInput
 parseFileContent = M.fromLists . map (map digitToInt) . lines
 
-sampleInput :: ProblemInput
-sampleInput = M.fromLists [[2,1,9,9,9,4,3,2,1,0],
-                           [3,9,8,7,8,9,4,9,2,1],
-                           [9,8,5,6,7,8,9,8,9,2],
-                           [8,7,6,7,8,9,6,7,8,9],
-                           [9,8,9,9,9,6,5,6,7,8]]
-
 allowedDirs :: [Coord]
 allowedDirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
