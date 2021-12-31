5ebf461b05c42ea8758e629c1a84be21cbd7a78d
Ollenurb <brunello.mtt@gmail.com>
2021-12-31 02:57:05 +0100

    Day13 - Part 1

--- solutions/Day13.hs
+++ solutions/Day13.hs
@@ -1,6 +1,8 @@
 module Day13 where
 
 -- Needed imports
+import Data.List (nub)
+import Data.List.Split (splitOn)
 
 -- Do not change. The main program will access the solutions from this function
 solve :: String -> String
@@ -11,16 +13,64 @@ solve fcontent = "Solution 1:\t" ++ sol1 ++ "\nSolution2:\t" ++ sol2 ++ "\n"
 
 ------------------------------------------------------------------------------
 -- Change according to the problem
-type ProblemInput = String
+type ProblemInput = (Paper, [Command])
+type Paper   = [(Int, Int)]
+data Axis    = X | Y deriving (Show, Eq)
+data Command = Command Axis Int
+    deriving (Show, Eq)
+
+testInput :: Paper
+testInput = [(6,10),
+             (0,14),
+             (9,10),
+             (0,3),
+             (10,4),
+             (4,11),
+             (6,0),
+             (6,12),
+             (4,1),
+             (0,13),
+             (10,12),
+             (3,4),
+             (3,0),
+             (8,4),
+             (1,10),
+             (2,14),
+             (8,10),
+             (9,0)]
+
+foldPaper :: Command -> Paper -> Paper
+foldPaper command = nub . map (translate command)
+
+translate :: Command -> (Int, Int) -> (Int, Int)
+translate (Command Y split) p@(x, y) = if y > split then (x, (2 * split) - y) else p
+translate (Command X split) p@(x, y) = if x > split then ((2 * split) - x, y) else p
 
 -- Parse the input file
 parseFileContent :: String -> ProblemInput
-parseFileContent = undefined
+parseFileContent content = (map parsePoint paper, map parseCommand commands)
+    where [paper, commands] = (splitOn [""] . lines) content
+
+parsePoint :: String -> (Int, Int)
+parsePoint x = read $ "(" ++ map transform x ++ ")"
+    where transform x = if x == '-' then ',' else x
+
+parseCommand :: String -> Command
+parseCommand = parseWord . last . words
+    where parseWord (d:'=':n) = Command (mkAxis d) (read n)
+          mkAxis 'x' = X
+          mkAxis 'y' = Y
 
 -- Solve the first part
 s1 :: ProblemInput -> Int
-s1 problemInput = -1
+s1 (paper, commands) = length $ foldPaper (head commands) paper
 
 -- Solve the second part
+-- Here you need to render the resulting fold
 s2 :: ProblemInput -> Int
-s2 problemInput = -1
+s2 (paper, commands) = length $ foldl (flip foldPaper) paper commands
+
+s2' (paper, commands) = foldl (flip foldPaper) paper commands
+
+render :: Paper -> IO ()
+render = undefined
