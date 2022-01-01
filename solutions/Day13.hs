b1a20835b1b25dcb9a2f555124edda38bf903e43
Ollenurb <brunello.mtt@gmail.com>
2022-01-01 22:18:46 +0100

    Day 13 - Finished

--- solutions/Day13.hs
+++ solutions/Day13.hs
@@ -1,55 +1,35 @@
 module Day13 where
 
 -- Needed imports
-import Data.List (nub)
+import Data.List (maximum)
+import qualified Data.HashSet as HS
 import Data.List.Split (splitOn)
 
 -- Do not change. The main program will access the solutions from this function
 solve :: String -> String
-solve fcontent = "Solution 1:\t" ++ sol1 ++ "\nSolution2:\t" ++ sol2 ++ "\n"
+solve fcontent = "Solution 1:\t" ++ sol1 ++ "\nSolution 2:\n" ++ sol2 ++ "\n"
     where problemInput = parseFileContent fcontent
           sol1 = (show . s1) problemInput
-          sol2 = (show . s1) problemInput
+          sol2 = (render . s2) problemInput
 
 ------------------------------------------------------------------------------
 -- Change according to the problem
 type ProblemInput = (Paper, [Command])
-type Paper   = [(Int, Int)]
+type Paper   = HS.HashSet (Int, Int)
 data Axis    = X | Y deriving (Show, Eq)
 data Command = Command Axis Int
     deriving (Show, Eq)
 
-testInput :: Paper
-testInput = [(6,10),
-             (0,14),
-             (9,10),
-             (0,3),
-             (10,4),
-             (4,11),
-             (6,0),
-             (6,12),
-             (4,1),
-             (0,13),
-             (10,12),
-             (3,4),
-             (3,0),
-             (8,4),
-             (1,10),
-             (2,14),
-             (8,10),
-             (9,0)]
-
 foldPaper :: Command -> Paper -> Paper
-foldPaper command = nub . map (translate command)
-
-translate :: Command -> (Int, Int) -> (Int, Int)
-translate (Command Y split) p@(x, y) = if y > split then (x, (2 * split) - y) else p
-translate (Command X split) p@(x, y) = if x > split then ((2 * split) - x, y) else p
+foldPaper command = HS.map (translate command)
+    where
+        translate (Command Y split) p@(x, y) = if y > split then (x, (2 * split) - y) else p
+        translate (Command X split) p@(x, y) = if x > split then ((2 * split) - x, y) else p
 
 -- Parse the input file
 parseFileContent :: String -> ProblemInput
-parseFileContent content = (map parsePoint paper, map parseCommand commands)
-    where [paper, commands] = (splitOn [""] . lines) content
+parseFileContent content = (HS.fromList $ map parsePoint paper, map parseCommand commands)
+    where [paper, commands] = splitOn [""] . lines $ content
 
 parsePoint :: String -> (Int, Int)
 parsePoint x = read $ "(" ++ map transform x ++ ")"
@@ -67,10 +47,10 @@ s1 (paper, commands) = length $ foldPaper (head commands) paper
 
 -- Solve the second part
 -- Here you need to render the resulting fold
-s2 :: ProblemInput -> Int
-s2 (paper, commands) = length $ foldl (flip foldPaper) paper commands
+s2 :: ProblemInput -> Paper
+s2 (paper, commands) = foldl (flip foldPaper) paper commands
 
-s2' (paper, commands) = foldl (flip foldPaper) paper commands
-
-render :: Paper -> IO ()
-render = undefined
+render :: Paper -> String
+render p = unlines [[if (x, y) `HS.member` p then '#' else ' ' | x <- [0..xMax]] | y <- [0..yMax]]
+    where
+        (xMax, yMax) = (maximum . HS.map fst $ p, maximum . HS.map snd $ p)
